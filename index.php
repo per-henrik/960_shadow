0a794ccde2aee8aa191f8f911e851174fbde5b02
Per Oja <per@oja.no>
2011-05-24 11:30:32 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>untitled</title>
+	<meta name="generator" content="TextMate http://macromates.com/">
+	<meta name="author" content="Per Henrik Oja">
+	<!-- Date: 2011-05-23 -->
+  <link rel="stylesheet" type="text/css" media="all" href="../../960.gs/code/css/reset.css" />
+  <link rel="stylesheet" type="text/css" media="all" href="../../960.gs/code/css/text.css" />
+  <link rel="stylesheet" type="text/css" media="all" href="../../960.gs/code/css/960.css" />
+  <style type="text/css">
+  body {
+    font-family: "Helvetica","verdana";
+    text-transform:uppercase;
+  }
+  div.shadow{
+    background-image:url('download.png');
+    min-height:100px;
+    background-repeat:no-repeat;
+    background-position:top-left ; 
+    }  
+  .shadow .content {
+    text-align:center;
+    line-height:200%;
+    padding:0px;
+    margin:20px;
+    background-color:#fff;
+    min-height:200px;
+    border:solid 1px black;
+    overflow:hidden;
+  }
+  div.container_12 {
+    background-color:#59799D;
+  }
+  h1 {
+    text-align:center;
+    padding:40px 0 0 0;
+  }
+    </style>
+<body class="show-grid">
+<div class="container_12" style="padding:0">  
+  <div class="grid_12"> <div class="content"><h1>Demo of boxshadows</h1></div></div>  
+    <div class="clear"></div>  
+
+  <div class="grid_12 shadow"> <div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"></div>  
+  
+    <div class="grid_12 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"></div>  
+  
+    <div class="grid_7 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_5 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"></div>  
+  
+    <div class="grid_12  shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"></div>  
+  
+    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"></div>  
+
+    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"></div>  
+
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"></div>  
+  
+    <div class="grid_12  shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"></div>  
+  
+    <div class="grid_4 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_4 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_4 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"></div>  
+
+    <div class="grid_12 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="clear"> &nbsp;</div>  
+  
+</div>
+</body>
+</html>
