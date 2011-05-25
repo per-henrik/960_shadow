0498ed0d952d78524557846257ce4fe69f718640
Per Oja <per@oja.no>
2011-05-25 16:11:50 +0200

    First eMatch layout commit. Basic layout done

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
   body {
     font-family: "Helvetica","verdana";
     text-transform:uppercase;
+    background-color:white;
   }
   div.shadow{
     background-image:url('download.png');
@@ -22,79 +23,110 @@
     background-repeat:no-repeat;
     background-position:top-left ; 
     }  
-  .shadow .content {
+  .shadow div {
     text-align:center;
     line-height:200%;
-    padding:0px;
+    padding:0;
     margin:20px;
     background-color:#fff;
     min-height:200px;
-    border:solid 1px black;
-    overflow:hidden;
   }
   div.container_12 {
-    background-color:#59799D;
+    background-color: white;
+  }
+  .footer {
+    background-color:#978E72; 
+    color:white;
+    font-weight:bold;
   }
   h1 {
-    text-align:center;
     padding:40px 0 0 0;
   }
+  h1.img {
+    float:left;
+  }
+  .content, .tagline{
+    margin-bottom:10px;
+    overflow:hidden;
+    border:solid 1px black;
+  }
+  .border {
+    border:solid 1px black;
+  }
+  .center {
+    text-align:center;
+  }
+  .tagline {
+    background-color: #0792D1;
+    border:solid 1px #0792D1;
+    color:white;
+    font-weight:bold;
+    padding-left:2px;
+  }
+  .headerbox {
+    background-color:#8CBAE4 !important;
+    color:white;
+    font-size:50px;
+    margin-bottom: 10px;
+    height:154px;
+  }
+  .headerbox div {
+    vertical-align:text-bottom;
+    position:relative;
+    top:80px;
+    left:20px;
+  }
+  .header .grid_2,
+  .header .grid_10 {
+    margin-top:10px;
+  }
+  .header {
+    margin-bottom:10px;
+  }
+  
     </style>
 <body class="show-grid">
-<div class="container_12" style="padding:0">  
-  <div class="grid_12"> <div class="content"><h1>Demo of boxshadows</h1></div></div>  
+<div class="container_12 header" style="padding:0">  
+  <div class="grid_2"><div class=""><img src="ematch-logo-standing.png"></div></div>  
+  <div class="grid_10 headerbox"><div class="">Initiating Innovation!</div></div>  
+    <div class="clear"></div>  
+</div>
+
+<div class="container_12 " style="padding:0">  
+  <div class="grid_12 "> <div class="tagline">  International Profiling & Project Matchmaking in the ICT and Digital Media Sector
+  </div></div>  
+    <div class="clear"></div>  
+  
+    <div class="grid_12 "><div class=""><h2>Menyer?</h2></div></div>  
     <div class="clear"></div>  
 
-  <div class="grid_12 shadow"> <div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_4 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_4 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_4 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
     <div class="clear"></div>  
-  
-    <div class="grid_12 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+</div>
+
+<div class="container_12 magebelte" style="padding:0">  
+    <div class="grid_6 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_6 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+     <div class="clear"></div>  
+
+    <div class="grid_12 "><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
     <div class="clear"></div>  
-  
-    <div class="grid_7 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_5 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+
+    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
     <div class="clear"></div>  
+
   
     <div class="grid_12  shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
     <div class="clear"></div>  
   
-    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_3 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="clear"></div>  
 
-    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_2 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="clear"></div>  
-
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_1 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="clear"></div>  
-  
-    <div class="grid_12  shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="clear"></div>  
-  
-    <div class="grid_4 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_4 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="grid_4 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
-    <div class="clear"></div>  
-
-    <div class="grid_12 shadow"><div class="content"><img src="krasnodar-region-landscape.jpg"></div></div>  
+    <div class="grid_12 center footer"><p>Coordinated by <a href="http://www.infosector.no">Infosector AS</a><br>Storgata 3, Postboks 44 Sentrum, 0101 Oslo, Norway<br>
+    <a href="mailto:mail@infosector.no">mail@infosector.no</a>, Org. No: 971049707</p></div></div>  
     <div class="clear"> &nbsp;</div>  
   
 </div>
